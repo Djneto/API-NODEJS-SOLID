859655d9a0f304c490ab5ffc26357e753527e3a6
Djalma Farias Bastos Neto <df82835es@df.trf1.gov.br>
2023-06-16 16:33:51 -0300

    Testes E2E de rotas de academias

--- src/http/controllers/gyms/create.spec.ts
+++ src/http/controllers/gyms/create.spec.ts
@@ -3,7 +3,7 @@ import { app } from '@/app'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 
-describe('Profile (e2e)', () => {
+describe('Create Gym (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,20 +12,21 @@ describe('Profile (e2e)', () => {
     await app.close()
   })
 
-  it('Should be able to get user profile', async () => {
+  it('Should be able to create a gym', async () => {
 
     const { token } = await createAndAuthenticateUser(app)
 
-    const profileResponse = await request(app.server)
-    .get('/me')
+    const response = await request(app.server)
+    .post('/gyms')
     .set('Authorization', `Bearer ${token}`)
-    .send()
+    .send({
+      title: 'JavaScript Gym',
+      description: 'Some description',
+      phone: '9999999999',
+      latitude: -27.0747279,
+      longitude: -49.4889672
+    })
 
-    expect(profileResponse.statusCode).toEqual(200)
-    expect(profileResponse.body.user).toEqual(
-      expect.objectContaining({
-        email: 'john@gmail.com',
-      })
-    )
+    expect(response.statusCode).toEqual(201)
   })
 })
